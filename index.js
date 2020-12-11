922e6b0695a132329e8283c6bc10a04d4e829174
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-11 14:02:37 +0630

    Creating custom routing

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const http = require("http");
-
+const url = require("url");
 // create server
 const server = http.createServer((req, res) => {
-    console.log("Request reached");
-    res.end("Hello from node server");
+    console.log(req.url);
+    if (req.url === "/") {
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+            "my-headers": "ok",
+        });
+        res.end("<h1>Home</h1>");
+    } else if (req.url === "/products") {
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+            "my-headers": "ok",
+        });
+        res.end("<h1>Products</h1>");
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+            "my-headers": "ok",
+        }); 
+        res.end("<h1>Page not found</h1>");
+    }
 });
 
 // listen server
