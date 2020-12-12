ca000915d9fb501b29ac1e38aa226b7b14c85e98
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-12 16:01:05 +0630

    Creating overview page

--- index.js
+++ index.js
@@ -2,17 +2,35 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 // Top level code
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
-const datObj = JSON.parse(data);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const overviewTemplate = fs.readFileSync(`${__dirname}/templates/overviewTemplate.html`, "utf-8");
+const cardTemplate = fs.readFileSync(`${__dirname}/templates/cardTemplate.html`, "utf-8");
+const productTemplate = fs.readFileSync(`${__dirname}/templates/productTemplate.html`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, data) => {
+    let output = template.replace(/{%ID%}/g, data.id);
+    output = output.replace(/{%PRODUCT_NAME%}/g, data.productName);
+    output = output.replace(/{%IMAGE%}/g, data.image);
+    output = output.replace(/{%FROM%}/g, data.from);
+    output = output.replace(/{%NUTRIENTS%}/g, data.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, data.quantity);
+    output = output.replace(/{%PRICE%}/g, data.price);
+    output = output.replace(/{%DESCRIPTION%}/g, data.description);
+    if (!data.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+    return output;
+};
 // create server
 const server = http.createServer((req, res) => {
     console.log(req.url);
     if (req.url === "/") {
+        const cards = dataObj.map((el) => replaceTemplate(cardTemplate, el)).join("");
+        const overview = overviewTemplate.replace(/{%PRODUCT_CARDS%}/g, cards);
         res.writeHead(200, {
             "Content-Type": "text/html",
-            "my-headers": "ok",
         });
-        res.end("<h1>Home</h1>");
+        res.end(overview);
     } else if (req.url === "/products") {
         res.writeHead(200, {
             "Content-Type": "text/html",
